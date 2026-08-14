7110524d35b5459247220f29c878f4f24d64a988
Abrar Ahmad Tausif <tausifahmadabrar@gmail.com>
2026-08-14 12:44:19 +0600

    expression

---
+++ expression_evaluation.java
@@ -0,0 +1,6 @@
+public class expression_evaluation {
+    public static void main(String[] args) {
+        double result = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println("(25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)= " + result);
+    }
+}
