51a3937493e2c184cd913dc0ccd66f82707f856f
Abrar Ahmad Tausif <tausifahmadabrar@gmail.com>
2026-08-14 14:22:45 +0600

    print american flag

--- flag.java
+++ flag.java
@@ -1,3 +1,18 @@
 public class flag {
-    
+    public static void main(String[] args) {
+        String p1 = "* * * * * * ================================== \n";
+        String p2 = " * * * * * ================================== \n";
+        String p3 = "============================================== \n";
+        
+        
+        for (int i = 0; i < 4; i++) {
+            System.out.print(p1);
+            System.out.print(p2);
+        }
+        System.out.print(p1); 
+        
+        for (int i = 0; i < 6; i++) {
+            System.out.print(p3);
+        }
+    }
 }
